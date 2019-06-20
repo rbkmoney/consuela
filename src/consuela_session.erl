6f493a6f3b6366c7bed35aa7c6771ac2f97a049d
Andrew Mayorov <a.mayorov@rbkmoney.com>
2019-06-20 19:53:10 +0300

    Expose session lock delay to clients

--- src/consuela_session.erl
+++ src/consuela_session.erl
@@ -9,6 +9,7 @@
 -type name()     :: binary().
 -type nodename() :: inet:hostname().
 -type ttl()      :: seconds().
+-type delay()    :: seconds().
 -type behavior() :: release | delete.
 -type indexes()  :: #{create | modify => integer()}.
 
@@ -16,23 +17,26 @@
 -type seconds()  :: non_neg_integer().
 
 -type t() :: #{
-    id       := id(),
-    name     := name(),
-    node     := nodename(),
-    ttl      := ttl(),
-    behavior := behavior(),
-    indexes  := indexes()
+    id         := id(),
+    name       := name(),
+    node       := nodename(),
+    ttl        := ttl(),
+    lock_delay := delay(),
+    behavior   := behavior(),
+    indexes    := indexes()
 }.
 
 -export_type([id/0]).
 -export_type([name/0]).
 -export_type([nodename/0]).
 -export_type([ttl/0]).
+-export_type([delay/0]).
 -export_type([behavior/0]).
 -export_type([t/0]).
 
 -export([create/4]).
 -export([create/5]).
+-export([create/6]).
 -export([destroy/2]).
 -export([get/2]).
 -export([renew/2]).
@@ -42,7 +46,10 @@
 -spec create(name(), nodename(), ttl(), consuela_client:t()) ->
     {ok, id()}.
 
--spec create(name(), nodename(), ttl(), behavior(), consuela_client:t()) ->
+-spec create(name(), nodename(), ttl(), delay(), consuela_client:t()) ->
+    {ok, id()}.
+
+-spec create(name(), nodename(), ttl(), delay(), behavior(), consuela_client:t()) ->
     {ok, id()}.
 
 -spec get(id(), consuela_client:t()) ->
@@ -55,11 +62,20 @@
     {ok, t()}.
 
 create(Name, Node, TTL, Client) ->
-    create(Name, Node, TTL, release, Client).
+    create(#{name => Name, node => Node, ttl => TTL}, Client).
 
-create(Name, Node, TTL, Behavior, Client) ->
+create(Name, Node, TTL, LockDelay, Client) ->
+    create(#{name => Name, node => Node, ttl => TTL, lock_delay => LockDelay}, Client).
+
+create(Name, Node, TTL, LockDelay, Behavior, Client) ->
+    create(
+        #{name => Name, node => Node, ttl => TTL, lock_delay => LockDelay, behavior => Behavior},
+        Client
+    ).
+
+create(Params, Client) ->
     Resource = <<"/v1/session/create">>,
-    Content = encode_params({Name, Node, TTL, Behavior}),
+    Content = encode_params(Params),
     case consuela_client:request(put, Resource, Content, Client) of
         {ok, SessionID} ->
             {ok, decode_session_id(SessionID)};
@@ -98,13 +114,20 @@ renew(ID, Client) ->
 
 %%
 
-encode_params({Name, Node, TTL, Behavior}) ->
-    #{
-        <<"Name">>     => encode_name(Name),
-        <<"Node">>     => encode_nodename(Node),
-        <<"TTL">>      => encode_ttl(TTL),
-        <<"Behavior">> => encode_behavior(Behavior)
-    }.
+encode_params(Params = #{name := Name, node := Node, ttl := TTL}) ->
+    maps:fold(
+        fun
+            (behavior, V, R)   -> R#{<<"Behavior">> => encode_behavior(V)};
+            (lock_delay, V, R) -> R#{<<"LockDelay">> => encode_seconds(V)};
+            (_, _, R)          -> R
+        end,
+        #{
+            <<"Name">>     => encode_name(Name),
+            <<"Node">>     => encode_nodename(Node),
+            <<"TTL">>      => encode_seconds(TTL)
+        },
+        Params
+    ).
 
 encode_id(V) ->
     encode_binary(V).
@@ -117,7 +140,7 @@ encode_nodename(V) when is_atom(V) ->
 encode_nodename(V) when is_list(V) ->
     encode_string(V).
 
-encode_ttl(V) ->
+encode_seconds(V) ->
     encode_duration('s', V).
 
 encode_behavior(release) ->
@@ -145,19 +168,20 @@ decode_session(#{
     <<"Node">>        := Node,
     <<"Behavior">>    := Behavior,
     <<"TTL">>         := TTL,
+    <<"LockDelay">>   := LockDelay,
     <<"CreateIndex">> := CreateIndex,
     <<"ModifyIndex">> := ModifyIndex
     % TODO
     % <<"Checks">>    := Checks,
-    % <<"LockDelay">> := LockDelay,
 }) ->
     #{
-        id       => decode_id(ID),
-        name     => decode_name(Name),
-        node     => decode_nodename(Node),
-        behavior => decode_behavior(Behavior),
-        ttl      => decode_ttl(TTL),
-        indexes  => #{
+        id         => decode_id(ID),
+        name       => decode_name(Name),
+        node       => decode_nodename(Node),
+        behavior   => decode_behavior(Behavior),
+        ttl        => decode_seconds(TTL),
+        lock_delay => decode_seconds(LockDelay),
+        indexes    => #{
             create => decode_index(CreateIndex),
             modify => decode_index(ModifyIndex)
         }
@@ -172,7 +196,7 @@ decode_name(V) ->
 decode_nodename(V) ->
     decode_string(V).
 
-decode_ttl(V) ->
+decode_seconds(V) ->
     decode_duration('s', V).
 
 decode_behavior(<<"release">>) ->
@@ -183,6 +207,8 @@ decode_behavior(<<"delete">>) ->
 decode_index(V) ->
     decode_integer(V).
 
+decode_duration(U, V) when is_integer(V) ->
+    V div consuela_duration:factor(U);
 decode_duration(U, V) ->
     consuela_duration:parse(V) div consuela_duration:factor(U).
 
