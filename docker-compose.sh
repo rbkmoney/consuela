6089aaa02742210e25a383b27a13f34b05f2cd55
Andrey Fadeev <me@ciiol.net>
2020-04-03 15:56:50 +0300

    Remove consul ports binding to localhost
    This can prevent the parallel testing of different commits on CI workers with insufficient tasks isolation.

--- docker-compose.sh
+++ docker-compose.sh
@@ -14,31 +14,16 @@ services:
     networks:
       - consul
     hostname: consul1
-    ports:
-      - "8401:8400"
-      - "8501:8500"
-      - "8601:8600"
-      - "8601:8600/udp"
     command:
       agent -server -retry-join consul0 -client 0.0.0.0
 
   consul2:
     <<: *consul-server
     hostname: consul2
-    ports:
-      - "8402:8400"
-      - "8502:8500"
-      - "8602:8600"
-      - "8602:8600/udp"
 
   consul0:
     <<: *consul-server
     hostname: consul0
-    ports:
-      - "8400:8400"
-      - "8500:8500"
-      - "8600:8600"
-      - "8600:8600/udp"
     command:
       agent -server -bootstrap-expect 3 -ui -client 0.0.0.0
 
